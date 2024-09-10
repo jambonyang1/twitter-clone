d2dcce5d8a6a1e41f6c7b8e0c9c199e3db3974a3
jambonyang1 <1106wjdgus@gmail.com>
2024-09-10 14:52:11 +0900

    계정 생성 초기 세팅

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,106 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
 const CreateAccount = () => {
-  return <h1>create account</h1>;
+  const [isLoading, setIsLoading] = useState(false);
+  // 나중에 form을 다룰수 있는 다른 패키지로 리팩토링하기
+  // velog글, 작가 hdpark : 맨날 하는 폼, 좀만 더 쉽게 해보자 React-hook-form
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      // create an account
+      // set the name of the user
+      // redirect to the homepage
+    } catch (e) {
+      // set error
+    } finally {
+      setIsLoading(false);
+    }
+    console.log(name, email, password);
+  };
+  return (
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+        ></Input>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+        ></Input>
+        <Input
+          onChange={onChange}
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+        ></Input>
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        ></Input>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default CreateAccount;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+`;
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: ;
+    }
+  }
+`;
