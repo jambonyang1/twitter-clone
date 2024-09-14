2192ad0561adace7fb04957969c78cf3a131dd92
jambonyang1 <1106wjdgus@gmail.com>
2024-09-14 14:32:17 +0900

    navigation 바 제작 및 레이아웃 구성

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,23 @@
 import { Outlet } from "react-router-dom";
-
+import styled from "styled-components";
+import NavigationBar from "./navigation-bar";
 const Layout = () => {
   return (
-    <>
-      <h2>layout</h2>
+    <Wrapper>
+      <NavigationBar />
       <Outlet />
-    </>
+    </Wrapper>
   );
 };
 
 export default Layout;
+
+const Wrapper = styled.div`
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 1fr 4fr;
+  height: 100%;
+  padding: 50px 0px;
+  width: 100%;
+  max-width: 860px;
+`;
