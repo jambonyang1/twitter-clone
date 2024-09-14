96f44abcd5504d013d80164247d204a09f555d74
jambonyang1 <1106wjdgus@gmail.com>
2024-09-14 22:11:04 +0900

    타임라인

---
+++ src/components/timeline.tsx
@@ -0,0 +1,51 @@
+import { getDocs, orderBy, query, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { db } from "../firebase";
+import Tweet from "./tweet";
+
+export interface ITweet {
+  id: string;
+  photo: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+}
+
+const Timeline = () => {
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const fetchTweets = async () => {
+    const tweetsQuery = query(
+      collection(db, "tweets"),
+      orderBy("createdAt", "desc")
+    );
+    const snapshot = await getDocs(tweetsQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return (
+    <Wrapper>
+      {tweets.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </Wrapper>
+  );
+};
+
+export default Timeline;
+
+const Wrapper = styled.div``;
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,21 @@
 import PostTweetForm from "../components/post-tweet-form";
 import styled from "styled-components";
+import Timeline from "../components/timeline";
 
 const Home = () => {
   return (
     <Wrapper>
       <PostTweetForm />
+      <Timeline />
     </Wrapper>
   );
 };
 
 export default Home;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  grid-template-rows: 1fr 5fr;
+`;
