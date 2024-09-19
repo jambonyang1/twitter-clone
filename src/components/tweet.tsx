3f8b44c07fdc7ac522c146d3f69691cea8adb104
jambonyang1 <1106wjdgus@gmail.com>
2024-09-19 23:49:10 +0900

    프로필 사진 및 내가 작성한 트윗 보기 기능

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -111,8 +111,8 @@ const Tweet = ({ username, photo, tweet, userId, id }: ITweet) => {
                     aria-hidden="true"
                   >
                     <path
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
+                      clipRule="evenodd"
+                      fillRule="evenodd"
                       d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z"
                     ></path>
                   </svg>
